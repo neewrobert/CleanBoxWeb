de9874dd29c76719581e4c08c4de7d6519407d7a
Newton <neewrobert@gmail.com>
2019-10-23 01:45:21 -0300

    server.js

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
+//Install express server
 const express = require('express');
+const app = express();
+const path = require('path');
 
-const path = require('path');   
-
-const app = express();   
-
-// Serve only the static files form the dist directory    
-app.use(express.static(__dirname + '/dist'));
-
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/CleanBox'));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.get('/*', function(req,res) {  
-    res.sendFile(path.join(__dirname+'/dist/index.html'));   
+    res.sendFile(path.join(__dirname +'/dist/CleanBox/index.html'));   
 });  
-
-// Start the app by listening on the default Heroku port    
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
